5cda60736862173d4e591aa588f514f8cfa3b002
vaidas-lungis <vaidas.lungis+github@gmail.com>
2018-07-31 23:37:53 +0300

    Update hello/main.go to receive http request

--- hello/main.go
+++ hello/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
@@ -8,10 +10,10 @@ type Response struct {
 	Message string `json:"message"`
 }
 
-func Handler() (Response, error) {
-	return Response{
-		Message: "Go Serverless v1.0! Your function executed successfully!",
-	}, nil
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Println("Received body: ", request.Body)
+
+	return events.APIGatewayProxyResponse{Body: request.Body, StatusCode: 200}, nil
 }
 
 func main() {
